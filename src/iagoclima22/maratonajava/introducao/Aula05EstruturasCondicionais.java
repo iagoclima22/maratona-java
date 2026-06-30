2ebfb2bcd45486532c382a08cb0af2584b922441
Iago Lima <iagoclima22@gmail.com>
2026-06-29 21:53:41 -0300

    20 - Estruturas Condicionais pt 01 - IF

---
+++ src/iagoclima22/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package iagoclima22.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+        boolean isAutorizadoComprarBebiba = idade >= 18;
+        // !
+
+        if (isAutorizadoComprarBebiba) {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }
+
+        if(!isAutorizadoComprarBebiba){
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
