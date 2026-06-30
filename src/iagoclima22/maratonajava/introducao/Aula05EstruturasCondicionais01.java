c6bc7265cecffeff44e6158743f7421dc623e7b4
Iago Lima <iagoclima22@gmail.com>
2026-06-29 23:13:06 -0300

    21 - Estruturas Condicionais pt 02 - ELSE IF

--- src/iagoclima22/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/iagoclima22/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,6 +1,6 @@
 package iagoclima22.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoComprarBebiba = idade >= 18;
